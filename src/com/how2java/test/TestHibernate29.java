ee3c819160a6e1220674a0412c78b009f1c7acb3
陶文猛 <15152806231@163.com>
2019-05-16 16:25:18 +0800

    两种Session方式：getCurrentSession在提交事务后，session自动关闭，34行，在事务关闭后，试图关闭session,就会报session已经关闭的异常

---
+++ src/com/how2java/test/TestHibernate29.java
@@ -0,0 +1,40 @@
+package com.how2java.test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+
+import com.how2java.pojo.Category;
+import com.how2java.pojo.Product;
+import com.how2java.pojo.User;
+
+/**
+ * 
+ */
+public class TestHibernate29 {
+
+	public static void main(String[] args) throws InterruptedException {
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        
+        Session s1 = sf.getCurrentSession();
+ 
+        s1.beginTransaction();
+ 
+        s1.get(Product.class, 5);
+ 
+        s1.getTransaction().commit();
+         
+        s1.close();
+ 
+        sf.close();
+		
+	}
+		
+}
