475e5fa295d8105d363c38a22d0b345b341c83d5
陶文猛 <15152806231@163.com>
2019-05-16 16:20:29 +0800

    两种Session方式：openSession查询时候不需要事务 如果是做增加，修改，删除是必须放在事务里进行的。 但是如果是查询或者get，那么对于openSession而言就不需要放在事务中进行

---
+++ src/com/how2java/test/TestHibernate27.java
@@ -0,0 +1,36 @@
+package com.how2java.test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+
+import com.how2java.pojo.Category;
+import com.how2java.pojo.Product;
+import com.how2java.pojo.User;
+
+/**
+ * 
+ */
+public class TestHibernate27 {
+
+	public static void main(String[] args) throws InterruptedException {
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        
+        Session s1 = sf.openSession();
+ 
+        s1.get(Product.class, 5);
+ 
+        s1.close();
+ 
+        sf.close();
+		
+	}
+		
+}
