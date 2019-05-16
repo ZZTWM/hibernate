50758094cb0ee3a6c2ddaab3ef6c9ca08776219a
陶文猛 <15152806231@163.com>
2019-05-16 14:25:04 +0800

    delete级联

---
+++ src/com/how2java/test/TestHibernate16.java
@@ -0,0 +1,40 @@
+package com.how2java.test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+
+import com.how2java.pojo.Category;
+import com.how2java.pojo.Product;
+import com.how2java.pojo.User;
+
+/**
+ * 
+ */
+public class TestHibernate16 {
+	
+	public static void main(String[] args) {
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		
+		Session s = sf.openSession();
+		
+		s.beginTransaction();
+		
+		Category c = (Category) s.get(Category.class, 1);
+		s.delete(c);
+		
+		s.getTransaction().commit();
+		
+		s.close();
+		sf.close();
+		
+	}
+	
+}
